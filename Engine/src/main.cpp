e24c0d17b47445cef4cf18a41ae06ff1b92189ca
guiyrt <danielregado@gmail.com>
2020-02-25 18:20:43 +0000

    Engine: - Correção no CMakeLists.txt - Inicio de desenvolvimento do Engine em src/main.cpp

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -1,91 +1,23 @@
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
+#include <iostream>
+#include "tinyxml2.h"
 
-#include <math.h>
+using namespace std;
 
-void changeSize(int w, int h) {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        cerr << "Engine requer exatamente 1 argumento, o ficheiro de configuração XML." << endl;
+        cerr << "Uso ./engine <file.xml>" << endl;
+        return 1;
+    }
 
-	// Prevent a divide by zero, when window is too short
-	// (you cant make a window with zero width).
-	if(h == 0)
-		h = 1;
+    tinyxml2::XMLDocument config;
 
-	// compute window's aspect ratio 
-	float ratio = w * 1.0 / h;
-
-	// Set the projection matrix as current
-	glMatrixMode(GL_PROJECTION);
-	// Load Identity Matrix
-	glLoadIdentity();
-	
-	// Set the viewport to be the entire window
-    glViewport(0, 0, w, h);
-
-	// Set perspective
-	gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
-
-	// return to the model view matrix mode
-	glMatrixMode(GL_MODELVIEW);
-}
-
-
-void renderScene(void) {
-
-	// clear buffers
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	// set the camera
-	glLoadIdentity();
-	gluLookAt(5.0,5.0,5.0, 
-		      0.0,0.0,0.0,
-			  0.0f,1.0f,0.0f);
-
-// put the geometric transformations here
-
-
-// put drawing instructions here
-
-
-	// End of frame
-	glutSwapBuffers();
-}
-
-
-
-// write function to process keyboard events
-
-
-
-
-
-
-int main(int argc, char **argv) {
-
-// init GLUT and the window
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
-	glutInitWindowPosition(100,100);
-	glutInitWindowSize(800,800);
-	glutCreateWindow("CG@DI-UM");
-		
-// Required callback registry 
-	glutDisplayFunc(renderScene);
-	glutReshapeFunc(changeSize);
-
-	
-// put here the registration of the keyboard callbacks
-
-
-
-//  OpenGL settings
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-	
-// enter GLUT's main cycle
-	glutMainLoop();
-	
-	return 1;
+    /*
+    tinyxml2::XMLError result = config.LoadFile(argv[2]);
+    if (result != tinyxml2::XML_SUCCESS) {
+        cerr << "Ocorreu um erro na leitura do ficheiro de configuração XML fornecido." << endl;
+        return 1;
+    }
+    cout << "YAY" << endl;*/
+    return 0;
 }
