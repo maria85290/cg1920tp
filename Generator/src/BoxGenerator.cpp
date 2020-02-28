5062a1eb6988dbd9982d3b9dd915e7769f85077f
maria85290 <49396253+maria85290@users.noreply.github.com>
2020-02-28 23:55:32 +0000

    Adicionei a box

--- Generator/src/BoxGenerator.cpp
+++ Generator/src/BoxGenerator.cpp
@@ -3,7 +3,6 @@
 #include <string>
 #include "BoxGenerator.h"
 #include "PlaneGenerator.h"
-
 bool BoxGenerator::ParseArguments(int argc, char* argv[]) {
    
     // ./generator box x y z box.3d
@@ -15,13 +14,97 @@ bool BoxGenerator::ParseArguments(int argc, char* argv[]) {
 
     SetFilename(argv[5]); // Número correto de argumentos passado
 
-    dimX_ = stoi(argv[2]) ;
-    dimY_ = stoi(argv[3]);;
-    dimZ_ = stoi(argv[4]);;
+    dimX_ = stoi(argv[2]);
+    dimY_ = stoi(argv[3]);
+    dimZ_ = stoi(argv[4]);
 
     return true;
 }
 
+
 void BoxGenerator::GenerateVertices() {
- 
+   
+    //static PlaneGenerator pbase;
+    //pbase.incializarVar(2, dimX_, dimY_,0,0,0);
+    //pbase.GenerateVertices();
+
+
+
+
+    float y_ = - dimY_/2.0;
+    float h = dimZ_/2.0;
+    float w = dimX_/2.0;
+    
+    // DESENHAR A BASE - plano XZ
+
+    AddVertex(-w, y_, -h);
+    AddVertex(w, y_, h);
+    AddVertex(-w, y_, h);
+
+    AddVertex(-w, y_, -h);
+    AddVertex(w, y_, -h);
+    AddVertex(w, y_, h);
+
+
+    // PARTE SUPERIOR - plano ZX
+    y_ = dimY_ / 2.0;
+    AddVertex(-w, y_, -h);
+    AddVertex(-w, y_, h);
+    AddVertex(w, y_, h);
+
+    AddVertex(-w, y_, -h);
+    AddVertex(w, y_, h);
+    AddVertex(w, y_, -h);
+
+    //PARTE FRONTAL - plano XY:
+    float z_ = dimZ_ / 2.0;
+    w = dimX_/2.0;
+    h = dimY_ / 2.0;
+    AddVertex(w, h, z_);
+    AddVertex(-w, h, z_);
+    AddVertex(-w, -h, z_);
+
+    AddVertex(w, h, z_);
+    AddVertex(-w, -h, z_);
+    AddVertex(w, -h, z_);
+
+
+    // Parte de tras plano yx
+    z_ = -dimZ_ / 2.0;
+    AddVertex(w, h, z_);
+    AddVertex(-w, -h, z_);
+    AddVertex(-w, h, z_);
+
+    AddVertex(w, h, z_);
+    AddVertex(w, -h, z_);
+    AddVertex(-w, -h, z_);
+
+    //lado direito - plano YZ:
+    float x_ = dimX_ / 2.0;
+    h = dimY_ / 2.0;
+    w = dimZ_ / 2.0;
+
+    AddVertex(x_, h, w);
+    AddVertex(x_, -h, w);
+    AddVertex(x_, -h, -w);
+
+    AddVertex(x_, h, w);
+    AddVertex(x_, -h, -w);
+    AddVertex(x_, h, -w);
+
+    //lado esquerdo plano zy
+
+    x_ = -dimX_ / 2.0;
+
+    AddVertex(x_, h, w);
+    AddVertex(x_, -h, -w);
+    AddVertex(x_, -h, w);
+
+    AddVertex(x_, h, w);
+    AddVertex(x_, h, -w);
+    AddVertex(x_, -h, -w);
+
+
 }
+
+
--- Generator/src/BoxGenerator.h
+++ Generator/src/BoxGenerator.h
@@ -2,13 +2,15 @@
 #define CG_TP_GENERATOR_SRC_BOXGENERATOR_H_
 
 #include "AbstractGenerator.h"
+#include "PlaneGenerator.h"
+
 
 class BoxGenerator : public AbstractGenerator {
 private:
     int dimX_;
     int dimY_;
     int dimZ_;
-    int numDiv_;
+   
 
 public:
     ~BoxGenerator() override = default;
